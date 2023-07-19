2e4a38075e7109d17c52c4b235af0cb2626431a6
lamcao2002 <caovohoanglam2002@gmail.com>
2023-07-19 20:56:38 +0700

    update path upload

--- build/config/upload.js
+++ build/config/upload.js
@@ -6,13 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.parserUploadLocal = void 0;
 const multer_1 = __importDefault(require("multer"));
 const fs_extra_1 = __importDefault(require("fs-extra"));
+const path_1 = __importDefault(require("path"));
 const storage = multer_1.default.diskStorage({
     destination: function (req, file, cb) {
-        const path = `./resources/${req.query.projectName}/present`;
-        if (!fs_extra_1.default.existsSync(path)) {
-            fs_extra_1.default.mkdirSync(path, { recursive: true });
+        const path1 = `./resources/${req.query.projectName}/present`;
+        const dir = path_1.default.join(__dirname, "../../resources/", req.query.projectName, "present");
+        if (!fs_extra_1.default.existsSync(dir)) {
+            fs_extra_1.default.mkdirSync(dir, { recursive: true });
         }
-        cb(null, path);
+        cb(null, dir);
     },
     filename: function (req, file, cb) {
         const fileExtension = file.mimetype.split("/")[1];
--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,15 +1,21 @@
 import multer from "multer";
 import fs from "fs-extra";
+import path from "path";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    const path = `./resources/${req.query.projectName}/present`;
-    
-    if (!fs.existsSync(path)) {
-      fs.mkdirSync(path, { recursive: true })
+    const path1 = `./resources/${req.query.projectName}/present`;
+    const dir = path.join(
+      __dirname,
+      "../../resources/",
+      req.query.projectName as string,
+      "present"
+    );
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true })
     }
 
-    cb(null, path)
+    cb(null, dir)
   },
   filename: function (req, file, cb) {
     const fileExtension = file.mimetype.split("/")[1];
